bcdbe09a099842a5ceba81d848ff944669126bfd
Evanyl <55635347+Evanyl@users.noreply.github.com>
2020-07-07 21:46:20 -0700

    Add files via upload

--- src/input.js
+++ src/input.js
@@ -37,12 +37,13 @@ export default class InputHandler {
 		});
 
 		htmlObject.addEventListener('touchend', (e) => {
+			alert(e.touches.[e.touches.length - 1].clientX);
 			if(e.touches[e.touches.length - 1].clientX < 400) {
-				if (paddle.speed > 0) {
+				if (paddle.speed < 0) {
 					paddle.speed = 0;
 				}
 			} else if (e.touches[e.touches.length -1].clientX > 600) {
-				if (paddle.speed < 0) {
+				if (paddle.speed > 0) {
 					paddle.speed = 0;
 				}
 			}
