99338c14e00a66c9b66e0a9108646a9e23e1b561
Evanyl <55635347+Evanyl@users.noreply.github.com>
2020-07-11 20:56:19 -0700

    Update levels.js

--- src/levels.js
+++ src/levels.js
@@ -22,42 +22,27 @@ export default class LevelHandler {
 
 		return bricks;
 	}
-}
-
-export function buildLevel(level) {
-	let bricks = [];
-	let position = {};
-	level.forEach((row, rowIndex) => {
-		row.forEach((brick, brickIndex) => {
-			if (brick === 1) {
-				position = {
-					x: 80 * brickIndex,
-					y: 20 * rowIndex
+	
+	randomLevelGenerator() {
+		let bricks = [];
+		for (let rowIndex = 0; rowIndex < 20; rowIndex++) {
+			let row = [];
+			for (let columnIndex = 0; columnIndex < 10; columnIndex++) {
+				if (Math.random() < 0.5) {
+					row.push(1);
+				} else {
+					row.push(0);
 				}
-				bricks.push(new Brick(position));
 			}
-		});
-	});
-
-	return bricks;
-}
-
-export function randomLevelGenerator() {
-	let bricks = [];
-	for (let rowIndex = 0; rowIndex < 20; rowIndex++) {
-		let row = [];
-		for (let columnIndex = 0; columnIndex < 10; columnIndex++) {
-			if (Math.random() < 0.5) {
-				row.push(1);
-			} else {
-				row.push(0);
-			}
-		}
 		bricks.push(row);
 	}
 	
 	return bricks;
 }
+}
+
+
+
 
 
 // 10 across, 20 up and down
